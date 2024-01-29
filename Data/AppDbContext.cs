0d3c584d5ee2f98ceff34c87cb5f244735ee0ae4
Anamarija Jozić <anjozic@vsite.hr>
2024-01-29 13:13:16 +0100

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PIN4.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+
+
+
+    
+    }
+}
